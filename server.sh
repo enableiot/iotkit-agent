c8dcef2e82e43b6db2a5ce9939fd951a9de64c12
Mark Chmarny <mark@chmarny.com>
2013-12-16 10:21:40 -0800

    Added forever

--- server.sh
+++ server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export APP_DIR="/home/username/iotkit-agent"
 export AGENT_LOG_FILE="./agent.log"
 export SERVER_MQTT_PORT=1883
 export SERVER_REST_PORT=8080
@@ -13,6 +14,8 @@ export BROKER_HOST_CERT="${BROKER_CERT_DIR}/client.crt"
 export BROKER_HOST_USR="username"
 export BROKER_HOST_PSW="password"
 
-node server
 
-# nohup node server &
+if [ $(ps aux | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
+then
+    forever start --sourceDir $APP_DIR server.js
+fi
