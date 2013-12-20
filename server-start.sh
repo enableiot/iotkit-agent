b34e6ecd2e063c098016af453f55e851b2c7afe7
mchmarny <mark@chmarny.com>
2013-12-20 05:08:54 -0800

    Refactored config to use simple env vars

--- server-start.sh
+++ server-start.sh
@@ -4,7 +4,7 @@
 if [ $(ps aux | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
 
-	export IOTKIT_AGENT_DIR="${IOTKIT_AGENT_DIR:?/iotkit-agent}"
+	export IOTKIT_AGENT_DIR="${IOTKIT_DIR}/iotkit-agent"
 	export AGENT_LOG_FILE="${IOTKIT_AGENT_DIR}/agent.log"
 	export SERVER_MQTT_PORT=1883
 	export SERVER_REST_PORT=8080
@@ -12,11 +12,10 @@ then
 	export BROKER_HOST="data.enableiot.com"
 	export BROKER_PORT=8884
 	export BROKER_DATA_TOPIC="data"
-	export BROKER_CERT_DIR="${IOTKIT_CERTS_DIR:?certs}"
-	export BROKER_HOST_KEY="${BROKER_CERT_DIR}/client.key"
-	export BROKER_HOST_CERT="${BROKER_CERT_DIR}/client.crt"
-	export BROKER_HOST_USR="${IOTKIT_AGENT_USR:?username}"
-	export BROKER_HOST_PSW="${IOTKIT_AGENT_PSW:?password}"
+	export BROKER_HOST_KEY="${IOTKIT_AGENT_DIR}/certs/client.key"
+	export BROKER_HOST_CERT="${IOTKIT_AGENT_DIR}/certs/client.crt"
+	export BROKER_HOST_USR="${IOTKIT_BROKER_USR}"
+	export BROKER_HOST_PSW="${IOTKIT_BROKER_PSW}"
 	export CONSOLE_LOG_LEVEL="verbose"
 	export FILE_LOG_LEVEL="verbose"
 
