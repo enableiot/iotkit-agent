b5965facd61d9f336dea85e35d76c1f6c26d8d43
Hernan Bazan <hernan.bazan@intel.com>
2014-01-31 16:16:57 -0300

    Add current sensors list storage Support registration-status message

--- agent.js
+++ agent.js
@@ -1,24 +1,21 @@
-var mqtt = require('mqtt'),
-    os = require("os"),
-    fs =  require("fs"),
-    mac = require("getmac"),
-    express = require("express"),
-    winston = require('winston'),
-    dgram = require("dgram"),
-    logger = require("./lib/logger").init(),
+var logger = require("./lib/logger").init(),
     utils = require("./lib/utils").init(logger);
 
-var sensorList = {};
+var sensorsList = {};
 
 utils.getDeviceId(function(id){
 
     logger.info("IoT Kit Cloud Agent: ", id);
     var conf = utils.getConfig();
     var cloud = require("./lib/cloud").init(conf, logger, id);
+    
+    var sensorsListRepo = require("./lib/sensors-list-repository");
+    sensorsList = sensorsListRepo.getSensorsList() || {};
+    
     var agentMessage = require("./lib/agent-message");
-    agentMessage.init(logger, cloud, sensorList);
-
-    cloud.reg(sensorList);
+    agentMessage.init(logger, cloud, sensorsList);
+    
+    cloud.reg(sensorsList, agentMessage.registrationCompleted);
 
     var msgHandler = agentMessage.messageHandler;
 
