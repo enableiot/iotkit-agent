94da75e08f63e783d0dada2ed270b6ca853cc4e2
mchmarny <mark@chmarny.com>
2014-02-01 07:20:34 -0600

    merged off line changes in logger, web log view off by default, cleanup logging

--- agent.js
+++ agent.js
@@ -1,24 +1,28 @@
 var logger = require("./lib/logger").init(),
     utils = require("./lib/utils").init(logger);
 
-var sensorsList = {};
-
 utils.getDeviceId(function(id){
 
     logger.info("IoT Kit Cloud Agent: ", id);
     var conf = utils.getConfig();
     
+    // configure sensor store 
     var sensorsStore = require("./lib/sensors-store");
-    sensorsStore.init(logger);
-    sensorsList = sensorsStore.getSensorsList() || {};
+        sensorsStore.init(logger);
     
+    var sensorsList = sensorsStore.getSensorsList();
+    
+    // create a cloud connector
     var cloud = require("./lib/cloud").init(conf, logger, id, sensorsStore);
     
+    // configure message provider
     var agentMessage = require("./lib/agent-message");
-    agentMessage.init(logger, cloud, sensorsList);
+        agentMessage.init(logger, cloud, sensorsList);
     
+    // register device
     cloud.reg(sensorsList, agentMessage.registrationCompleted);
 
+    // create a local pub handler
     var msgHandler = agentMessage.messageHandler;
 
     logger.info("Starting listeners...");
@@ -30,3 +34,9 @@ utils.getDeviceId(function(id){
 });
 
 
+process.on("uncaughtException", function(err) {
+  logger.error("UncaughtException:", err.message);
+  logger.error(err.stack);
+  // let the process exit so that forever can restart it
+  process.exit(1); 
+});
