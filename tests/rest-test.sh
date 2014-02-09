a6574be3dea355f1bc07b4212517b05ce92a5983
mchmarny <mark@chmarny.com>
2014-02-09 06:14:57 -0800

    Updated samples to new format. This version WORKS

--- tests/rest-test.sh
+++ tests/rest-test.sh
@@ -2,5 +2,5 @@
 
 curl -i -X PUT http://127.0.0.1:9090 \
 	  -H 'Content-Type: application/json' \
-     --data '{"s": "mocked-sensor", "m": "air-temp", "v": 26.7}'
+     --data '{"s": "Temp-Sensor", "v": 26.7}'
 
--- tests/udp-test.sh
+++ tests/udp-test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-echo -n '{"s": "mocked-sensor", "m": "air-temp", "v": 26.7}' | \
-     nc -4u -w1 127.0.0.1 41234
+echo -n '{"s": "Temp-Sensor", "v": 26.7}' | nc -4u -w1 127.0.0.1 41234
