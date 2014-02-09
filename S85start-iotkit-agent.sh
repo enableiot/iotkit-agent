4208f78cfef15f6b293efb41a13507a3a4abc6d1
mchmarny <mark@chmarny.com>
2014-02-09 11:48:06 -0800

    Added UTC set to Galileo board

--- S85start-iotkit-agent.sh
+++ S85start-iotkit-agent.sh
@@ -3,5 +3,11 @@
 # This is to be copied into the /etc/rc5.d directory so it will be run 
 # by the init system at boot
 
+# update local time to UTC
+TM=$(curl -s http://www.timeapi.org/utc/now?format=%25Y-%25m-%25d%20%25H:%25M)
+echo $TM
+date --set="${TM}"
+
+# start agent
 cd /home/root/iotkit-agent
 ./start-agent.sh
