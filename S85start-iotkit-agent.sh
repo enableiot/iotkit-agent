45ed1acfe58f8a9967771c869b258c0f89722f0c
Patrick Holmes <patrick.j.holmes@gmail.com>
2014-02-10 11:26:46 -0800

    change date format and parameter to work with date in busybox

--- S85start-iotkit-agent.sh
+++ S85start-iotkit-agent.sh
@@ -4,9 +4,9 @@
 # by the init system at boot
 
 # update local time to UTC
-TM=$(curl -s http://www.timeapi.org/utc/now?format=%25Y-%25m-%25d%20%25H:%25M)
+TM=$(curl -s http://www.timeapi.org/utc/now?format=%25Y.%25m.%25d-%25H:%25M)
 echo $TM
-date --set="${TM}"
+date "${TM}"
 
 # start agent
 cd /home/root/iotkit-agent
