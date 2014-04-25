e84ba9fc004804fe9f8039097ebd475e622815a5
pachacutec <agustin.march@gmail.com>
2014-04-25 15:26:16 -0300

    fixed UT

--- test/lib_agentTests.js
+++ test/lib_agentTests.js
@@ -4,10 +4,7 @@ var assert = require('assert'),
     logger = require("../lib/logger").init(utils);
 
 describe('iotkit-agent', function() {
-   before(function() {
-     this.conf = utils.getConfig();
-   });
-    it('should generate a valid device Id', function(done) {
+     it('should generate a valid device Id', function(done) {
         utils.getDeviceId(function(id){
             assert(id, 'id is null');
             this.deviceId = id;
