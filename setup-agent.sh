689d82f607b65b364f2276fd5b8228512f66a0be
James Carr <james.carr@intel.com>
2014-05-29 21:24:42 -0400

    fixed typo in setup-agent.sh

--- setup-agent.sh
+++ setup-agent.sh
@@ -14,6 +14,19 @@ else
 	SUDO=
 fi
 
+# inject the file which starts the agent into the init process at run level 5
+echo "copying startup script"
+if [[ -d /etc/rc5.d/ ]]
+then
+	${SUDO} cp -f ./S85start-iotkit-agent.sh /etc/rc5.d/
+else
+	echo "no /etc/rc5.d directory - using new firmware"
+	${SUDO} cp -f startagent.service /etc/systemd/system
+	${SUDO} chmod 755 /etc/systemd/system/startagent.service
+	${SUDO} systemctl enable startagent.service
+	
+fi
+
 # installing local packages
 npm install --production
 
