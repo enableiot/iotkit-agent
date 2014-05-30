60ed79a154ceff1cc386819d72beb5dfadb464b3
James Carr <james.carr@intel.com>
2014-05-29 21:22:49 -0400

    more framework for new firmware. shouldnt break anything if old firmware

--- setup-agent.sh
+++ setup-agent.sh
@@ -14,15 +14,6 @@ else
 	SUDO=
 fi
 
-# inject the file which starts the agent into the init process at run level 5
-echo "copying startup script"
-if [[ -d /etc/rc5.d/ ]]
-then
-	${SUDO} cp -f ./S85start-iotkit-agent.sh /etc/rc5.d/
-else
-	echo "no /etc/rc5.d directory - startup script not copied"
-fi
-
 # installing local packages
 npm install --production
 
