ef4de8717db0c791909716ce52d23c40ba9ec9bf
pachacutec <agustin.m.march@intel.com>
2014-06-27 11:54:20 -0300

    sync code Signed-off-by: pachacutec <agustin.m.march@intel.com>

--- api/mqtt/connector.js
+++ api/mqtt/connector.js
@@ -27,13 +27,16 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 "use strict";
 var mqtt = require('mqtt');
 
-
 function Broker(conf, logger) {
     var me = this;
     me.host = conf.host;
     me.port = conf.port;
     me.secure = conf.secure;
-    me.crd = {};
+    me.keepalive = conf.keepalive || 60;
+    me.crd = {
+        username: conf.username,
+        password: conf.password
+    };
     me.max_retries = conf.retries || 30;
     me.messageHandler = [];
     me.logger = logger;
@@ -51,8 +54,7 @@ function Broker(conf, logger) {
         me.credential = {
             username: me.crd.username,
             password: me.crd.password,
-            keepalive: 59000,
-            clean: true
+            keepalive: me.keepalive,
         };
     };
     me.setCredential();
@@ -73,11 +75,11 @@ function Broker(conf, logger) {
         try {
            if ((me.client instanceof mqtt.MqttClient) === false) {
                if (me.secure === false) {
-                   me.logger.info("Non Secure Connection to ", me.host, ":", me.port);
+                   me.logger.info("Non Secure Connection to "+ me.host + ":" + me.port);
                    me.client = mqtt.createClient(me.port, me.host, me.credential);
                } else {
-                   me.logger.info("Trying with Secure Connection to", me.host, ":", me.port);
-                   me.logger.debug("with ", me.credential);
+                   me.logger.info("Trying with Secure Connection to" + me.host + ":" + me.port);
+                   me.logger.debug("with " + JSON.stringify(me.credential));
                    me.client = mqtt.createSecureClient(me.port, me.host, me.credential);
                 }
             }
@@ -88,16 +90,16 @@ function Broker(conf, logger) {
         function waitForConnection() {
             if (!me.client.connected) {
                 retries++;
-                me.logger.info("Waiting for MQTTConnector to connect # ", retries);
+                me.logger.info("Waiting for MQTTConnector to connect # " + retries);
                 if (retries < me.max_retries) {
                     setTimeout(waitForConnection, 1500);
                 } else {
-                    me.logger.info('MQTTConnector: Error Connecting to ', me.host, ':', me.port);
+                    me.logger.info('MQTTConnector: Error Connecting to '+  me.host + ':' + me.port);
                     done(new Error("Connection Error", 1001));
                 }
                 return false;
             }
-            me.logger.info('MQTTConnector: Connection successful to ', me.host, ':', me.port);
+            me.logger.info('MQTTConnector: Connection successful to ' + me.host + ':' + me.port);
             me.listen();
             done(null);
             return true;
@@ -135,7 +137,7 @@ function Broker(conf, logger) {
         for (i = 0; i < length; i++ ){
             var obj = me.messageHandler[i];
             if (tryPattern(obj.t, topic)) {
-                me.logger.info('Fired STATUS: %s', topic, message);
+                me.logger.info('Fired STATUS: %s' + topic + JSON.stringify(message));
                 obj.h(topic, message);
             }
         }
@@ -147,11 +149,11 @@ function Broker(conf, logger) {
          */
         var toCallBack = callback;
         function connectCallback() {
-            me.logger.debug('Subscribing to: ', topic);
+            me.logger.debug('Subscribing to: ' + topic);
             me.client.subscribe(topic, function (err, granted) {
-                logger.debug("grant ", granted);
+                me.logger.debug("grant " + granted);
                 var topicAsPattern = granted[0].topic.replace(/\+/g, "[^<>]*");
-                logger.debug("grant ", topicAsPattern);
+                me.logger.debug("grant " + topicAsPattern);
                 me.attach(topicAsPattern, handler);
                if (toCallBack) {
                     toCallBack();
@@ -175,9 +177,9 @@ function Broker(conf, logger) {
         }
     };
     me.unbind = function (topic, callback) {
-        me.logger.debug('Unbinding from Topic : T => ', topic);
+        me.logger.debug('Unbinding from Topic : T => '+ topic);
         me.client.unsubscribe(topic, function() {
-            me.logger.debug('Unbound from Topic : T => ', topic);
+            me.logger.debug('Unbound from Topic : T => '+ topic);
             me.dettach(topic);
             if (callback) {
                 callback();
@@ -198,7 +200,7 @@ function Broker(conf, logger) {
             options = options || me.pubArgs;
         }
         function publishCallback() {
-            me.logger.debug('Publishing : T => ', topic, " MSG => ", message);
+            me.logger.debug('Publishing : T => ' + topic + " MSG => " + JSON.stringify(message));
             me.client.publish(topic, JSON.stringify(message), options, callback);
         }
         if (!me.connected()) {
@@ -227,3 +229,4 @@ module.exports.singleton = function (conf, logger) {
     }
     return broker;
 };
+module.exports.Broker = Broker;
