4413a303babcc450b9c1bd72c9e8a5865f121b8f
Andrzej Joskowski <andrzej.joskowski@intel.com>
2014-12-04 14:37:28 +0100

    DP-3181 : Added initializing token.json and sensor-list.json files for iotkit-agent.js

--- api/cloud.proxy.js
+++ api/cloud.proxy.js
@@ -26,16 +26,28 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 "use strict";
+
 var msg = require('../lib/cloud-message'),
     common = require('../lib/common'),
-    proxyConnector = require('../lib/proxies').getProxyConnector();
+    proxyConnector = require('../lib/proxies').getProxyConnector(),
+    fs = require('fs'),
+    path = require('path');
 
+var initializeFile = function(filepath, data) {
+    if(!fs.exists(filepath)) {
+        common.writeToJson(filepath, data);
+    }
+};
 function IoTKitCloud(conf, logger, deviceId, customProxy) {
     var me = this;
     me.logger = logger;
     me.filename = conf.token_file || "token.json";
     me.fullFilename = common.getTokenFileName(me.filename);
+    initializeFile(me.fullFilename, { "deviceToken" : false, "accountId" : false });
     me.secret = common.readFileToJson(me.fullFilename);
+    var dataDirectory = conf.data_directory || path.join(__dirname, '../data/');
+    var fullPathForSensorListFile = path.join(dataDirectory +  'sensor-list.json');
+    initializeFile(fullPathForSensorListFile, []);
     me.proxy = customProxy || proxyConnector;
     me.max_retries = conf.activation_retries || 10;
     me.deviceId = deviceId;
