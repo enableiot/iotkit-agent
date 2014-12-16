b1bf0d781e28a6995f64b6707283d91aa37b7e30
Pawel Zwanitaj <pawel.zwanitaj@intel.com>
2014-12-16 10:28:26 +0100

    DP-3122 - Change config loading strategy
    DP-3122 Fix configuration files
    
    DP-3122 Add writing to user configuration instead global
    
    DP-3122 Fix configuration files to default
    
    Signed-off-by: Pawel Zwanitaj <pawel.zwanitaj@intel.com>

--- test/lib_sensors-storeTests.js
+++ test/lib_sensors-storeTests.js
@@ -1,29 +1,29 @@
 /*
-Copyright (c) 2014, Intel Corporation
+ Copyright (c) 2014, Intel Corporation
 
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
 
-    * Redistributions of source code must retain the above copyright notice,
-      this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice,
-      this list of conditions and the following disclaimer in the documentation
-      and/or other materials provided with the distribution.
-    * Neither the name of Intel Corporation nor the names of its contributors
-      may be used to endorse or promote products derived from this software
-      without specific prior written permission.
+ * Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ * Neither the name of Intel Corporation nor the names of its contributors
+ may be used to endorse or promote products derived from this software
+ without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 var path = require('path'),
     assert =  require('chai').assert,
@@ -40,11 +40,11 @@ describe(fileToTest, function(){
         console.log(arguments);
     };
 
-
     describe("Stora shall Read/Write from source", function () {
         var common = {
             readFileToJson : function (){},
-            writeToJson: function(){}
+            writeToJson: function(){},
+            getConfig: function(){}
         };
         var toTest;
         before(function(done) {
@@ -57,13 +57,32 @@ describe(fileToTest, function(){
         });
 
         it('Shall load a data from filename specified >', function (done) {
-            var dataFile = [
-                {name: 1, type: 2, cid: 3},
-                {name: 21, type: 22, cid: 23},
-                {name: 31, type: 32, cid: 33}
-            ];
+            var dataFile = {
+                "activation_retries": 10,
+                "activation_code": null,
+                "device_id": false,
+                "device_name": false,
+                "device_loc": [
+                    88.34,
+                    64.22047,
+                    0
+                ],
+                "gateway_id": false,
+                "deviceToken": "",
+                "accountId": "",
+                "sensor_list": []
+            }
 
-            var storeName = "sensorTest-list.json";
+            var config = {
+                "data_directory": ""
+            }
+
+            var storeName = "deviceTest.json";
+
+            common.getConfig = function(){
+                return config;
+            };
+
             common.readFileToJson = function (fullPath) {
                 var str = "/data/" + storeName;
                 assert.isString(fullPath, "The fullname is not String");
@@ -72,12 +91,12 @@ describe(fileToTest, function(){
             };
             toTest.__set__("common", common);
             var store = toTest.init(storeName, logger);
-            assert.lengthOf(store.data, dataFile.length, "The Data load is not the same");
-            assert.deepEqual(store.data, dataFile, "The Data store are missing data");
+            assert.lengthOf(store.data, dataFile['sensor_list'].length, "The Data load is not the same");
+            assert.deepEqual(store.data, dataFile['sensor_list'], "The Data store are missing data");
             done();
         });
         it('Shall Initialize with Empty Array when not data were save >', function (done) {
-            var storeName = "sensorTest-list.json";
+            var storeName = "deviceTest.json";
             common.readFileToJson = function (fullPath) {
                 var str = "/data/" + storeName;
                 assert.isString(fullPath, "The fullname is not String");
@@ -90,7 +109,7 @@ describe(fileToTest, function(){
             done();
         });
         it('Shall save data to file in JSON format>', function (done) {
-            var storeName = "sensorTest-list.json";
+            var storeName = "deviceTest.json";
             var data = [];
             common.readFileToJson = function (fullPath) {
                 var str = "/data/" + storeName;
@@ -98,6 +117,7 @@ describe(fileToTest, function(){
                 assert.include(fullPath, str, "The store Name and Data is not include");
                 return null;
             };
+
             function checkArray(dataToSave) {
                 for (var i = 0; i < 100; i++) {
                     var d = i * 1000;
@@ -105,12 +125,11 @@ describe(fileToTest, function(){
                     assert.equal(dataToSave[i].name, sD.name, "The name is missing at store");
                     assert.equal(dataToSave[i].type, sD.type, "The Type is missing at store");
                     assert.lengthOf(dataToSave[i].cid, 36, "The cid has not the length expected");
-
                 }
             }
 
-            common.writeToJson = function (fullPath, dataToSave) {
-                var str = "/data/" + storeName;
+            common.saveToDeviceConfig = function (fullPath, dataToSave) {
+                var str = "sensor_list";
                 assert.isString(fullPath, "The fullname is not String");
                 assert.include(fullPath, str, "The store Name and Data is not include");
                 assert.lengthOf(dataToSave, 100, "Some Data is missing");
@@ -136,23 +155,37 @@ describe(fileToTest, function(){
     describe(" Store Intreface ", function () {
         var myComm;
         var toTest= null;
-        var storeName = "sensorTest-list.json";
+        var storeName = "deviceTest.json";
         before(function(done) {
             toTest = rewire(fileToTest);
             myComm = require('../lib/common');
-            var dataFile = [
-                {name: 1, type: 2, cid: 3},
-                {name: 21, type: 22, cid: 23},
-                {name: 31, type: 32, cid: 33}
-            ];
+            var dataFile = {
+                "activation_retries": 10,
+                "activation_code": null,
+                "device_id": false,
+                "device_name": false,
+                "device_loc": [
+                    88.34,
+                    64.22047,
+                    0
+                ],
+                "gateway_id": false,
+                "deviceToken": "",
+                "accountId": "",
+                "sensor_list": [
+                    {name: 1, type: 2, cid: 3},
+                    {name: 21, type: 22, cid: 23},
+                    {name: 31, type: 32, cid: 33} ]
+            }
+
 
             var f = path.join(__dirname, '../data/' + storeName);
             myComm.writeToJson(f, dataFile);
             done();
         });
         after(function(done){
-           toTest = null;
-           done();
+            toTest = null;
+            done();
         });
 
         it('Shall return a sensor by CID >', function (done) {
