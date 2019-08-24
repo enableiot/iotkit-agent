a59ede1c953a664beb2bb07d6c543f12beff99c0
Marcel Wagner <wagmarcel@web.de>
2019-08-24 18:24:05 +0000

    testsensor.js: updated * flexible config allows to create more sensor types and stream to multiple accounts at the same time * added winston logger * for k8s usage: Script first checks whether there is a file mounted at /etc/oisp/sensorSpecs.json and if not use the default temp sensor * allowing string sensor and adding random offset to inital string value
    Signed-off-by: Marcel Wagner <wagmarcel@web.de>

--- container/scripts/testsensor/testsensor.js
+++ container/scripts/testsensor/testsensor.js
@@ -4,47 +4,85 @@
 // TEST_SAMPLES: if set number of samples to send before terminating
 // COMPONENT_TYPE: name of the component type
 // COMPONENT_NAME: name of the component
+// LOG_LEVEL: log level
 "use strict";
 var dgram = require('dgram');
+var PD = require('probability-distributions');
+var winston = require('winston');
+var fs = require('fs');
+const specsFileName = '/etc/oisp/sensorSpecs.json';
 
-var PORT = 41234;
-var HOST = '127.0.0.1';
-var tempValue = 20;
-var componentType = "temperature.v1.0";
-var componentName = "temp";
-var testSamples;
+var logLevel = process.env.LOG_LEVEL;
+if (logLevel === undefined) {
+    logLevel = 'warn';
+}
+const logger = winston.createLogger({
+    level: logLevel,
+    transports: [
+	new winston.transports.Console()
+    ]
+})
+
+var default_port = 41234;
+var default_host = '127.0.0.1';
+var default_sensorSpecs = [
+    {
+	agents: [
+	    {
+		port: default_port,
+		host: default_host
+	    }
+	],
+	name: "tempSensor",
+	componentName: "temp",
+	componentType: "temperature.v1.0",
+	type: "number",
+	sigma: 0.3,
+	startValue: 20
+    }
+]
+
+var values = {};
 var numSamples = 0;
+var testSamples;
+
+var sensorSpecs = [];
+
+if (fs.existsSync(specsFileName)) {
+    var jsondata = fs.readFileSync(specsFileName);
+    sensorSpecs = JSON.parse(jsondata);
+    logger.info("SensorSpec found in /etc/oisp. Loaded: " + JSON.stringify(sensorSpecs));
+}
+else {
+    sensorSpecs = default_sensorSpecs;
+    logger.info("SensorSpec NOT found in /etc/oisp. Default: " + JSON.stringify(sensorSpecs));
+}
 
 if (process.env.TEST_SAMPLES) {
     testSamples = process.env.TEST_SAMPLES;
 }
-if (process.env.COMPONENT_TYPE) {
-    componentType = process.env.COMPONENT_TYPE;
-}
-if (process.env.COMPONENT_NAME) {
-    componentName = process.env.COMPONENT_NAME;
-}
 
-
-
-var registerComponent = function(componentType, name) {
-    var component = { "t": componentType, "n": name }
+var registerComponent = function(spec) {
+    var component = { "t": spec.componentType, "n": spec.componentName }
     var comp_message = new Buffer(JSON.stringify(component));
-    var client = dgram.createSocket('udp4');
-    client.send(comp_message, 0, comp_message.length, PORT, HOST, function(err, response) {
-        if (err) console.log("Error:", err);
-        client.close();
-        console.log("registering " + JSON.stringify(component));
+    spec.agents.forEach(function(agent){
+	var client = dgram.createSocket('udp4');
+	logger.verbose("Registring component: " + JSON.stringify(component) + " at url " + agent.host + ":" + agent.port);
+	client.send(comp_message, 0, comp_message.length, agent.port, agent.host, function(err, response) {
+            if (err) logger.error("Error: " + err);
+            client.close();
+	});
     });
 }
 
-var sendObservation = function(data, cb) {
+var sendObservation = function(spec, data, cb) {
     var message = new Buffer(JSON.stringify(data));
-
-    var client = dgram.createSocket('udp4');
-    client.send(message, 0, message.length, PORT, HOST, function(err, response) {
-        cb && cb(err, response);
-        client.close();
+    spec.agents.forEach(function(agent) {
+	var client = dgram.createSocket('udp4');
+	client.send(message, 0, message.length, agent.port, agent.host, function(err, response) {
+            cb && cb(err, response);
+            client.close();
+	});
     });
 }
 
@@ -52,24 +90,40 @@ var getRandomInteger = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//first register the temp component
-setTimeout(
-function() {registerComponent(componentType, componentName)}, 5000);
+//initialize all values
+sensorSpecs.forEach(function(spec) {
+    values[spec.name] = spec.startValue;
+});
 
-setTimeout(
-function(){ setInterval(function() {
-    //to be on the save side re-register. Agent will realize if already existing
-    registerComponent(componentType, componentName);
-    var telemetry = { "n": componentName, "v": tempValue };
-    sendObservation(telemetry, function(err, bytes) {
-        if (err) console.log("Error:", err);
-        console.log(telemetry)
-        var change = getRandomInteger(100, -100)
-        tempValue += change / 100.0
-        numSamples++;
-        if (testSamples && numSamples >= testSamples) {
-            console.log("Maximal number of testsamples reached. Terminating!")
-            process.exit(0);
-        }
-    })
-}, 5 * 1000)}, 10000)
+//first register the temp component
+sensorSpecs.forEach(function(spec) {
+    setTimeout(
+	function() {registerComponent(spec)}, 5000);
+});
+
+sensorSpecs.forEach(function(spec){
+    setTimeout(
+	function(){ setInterval(function() {
+	    //to be on the save side re-register. Agent will realize if already existing
+	    registerComponent(spec);
+	    var value = values[spec.name];
+	    if (spec.type === "string") {
+		value += Math.round(Math.random() * 100000000);
+		}
+	    var telemetry = { "n": spec.componentName, "v": value };
+	    sendObservation(spec, telemetry, function(err, bytes) {
+		if (err) logger.error("Error:" + err);
+		logger.info(telemetry)
+		if (spec.type === "number") {
+		    var change = PD.rnorm(1, 0, spec.sigma);
+		    value += change[0];
+		    values[spec.name] = value;
+		}
+		numSamples++;
+		if (testSamples && numSamples >= testSamples) {
+		    logger.info("Maximal number of testsamples reached. Terminating!")
+		    process.exit(0);
+		}
+	    })
+	}, 5 * 1000)}, 10000)
+});
